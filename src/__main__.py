34aa8b73759957973baf2d1c73fb0afcd1e9a42a
Mohcine Ait Taleb <mait-tal@e2r9p15.1337.ma>
2026-07-05 20:23:40 +0100

    fixe: flake/mypy

--- src/__main__.py
+++ src/__main__.py
@@ -2,8 +2,8 @@ from .Chunkers import TextChunker
 from .Chunk import Chunk
 
 
-def main():
-    file: str = "datasets_public/public/AnsweredQuestions/dataset_code_public.json"
+def main() -> None:
+    file = "datasets_public/public/AnsweredQuestions/dataset_code_public.json"
     text_chunker: TextChunker = TextChunker(2000, 20)
     result: list[Chunk] = text_chunker.chunk_file(file)
     print(result[:10], end="\n")
