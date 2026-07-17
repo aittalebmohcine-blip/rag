b822864c9235ffe57e27e4c6948a9591b6ca8fa7
Mohcine Ait Taleb <mait-tal@e3r4p14.1337.ma>
2026-07-17 12:34:29 +0100

    feat: add coverter chunk->minimalsource

--- src/Chunk.py
+++ src/Chunk.py
@@ -1,8 +1,18 @@
 from pydantic import BaseModel, Field
 
+from .MinimalSource import MinimalSource
+
 
 class Chunk(BaseModel):
     text: str
     start: int = Field(ge=0)
     end: int = Field(ge=0)
-    file_path: str | None = None
+    file_path: str
+
+    def to_minimal_source(self) -> MinimalSource:
+        return MinimalSource(
+            file_path=self.file_path,
+            first_character_index=self.start,
+            last_character_index=self.end,
+            text=self.text,
+        )
