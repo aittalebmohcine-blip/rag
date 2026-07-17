7803b5391b75228e7f9193eb57eea67e87418f6c
Mohcine Ait Taleb <mait-tal@e3r4p14.1337.ma>
2026-07-17 12:33:05 +0100

    feat: add MinimalSource model

---
+++ src/MinimalSource.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+
+class MinimalSource(BaseModel):
+    file_path: str
+    first_character_index: int
+    last_character_index: int
+
+    # extra field
+    text: str
